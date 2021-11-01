7dbac0a4da185267c0051fa9dbbf8ff2a33f29d4
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-01 21:19:59 +0300

    feat: add useDebounce

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import useInput from './hooks/useInput';
 import Hover from './components/Hover';
 import List from './components/List';
+import useDebounce from './hooks/useDebounce';
 
 function App() {
   const Username = useInput('');
   const Password = useInput('');
 
+  const [value, setValue] = useState('');
+  const debouncedCallback = useDebounce(search, 500);
+  function search(query) {
+    fetch(`https://jsonplaceholder.typicode.com/todos` + query)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      });
+  }
+
+  const handleChange = e => {
+    setValue(e.target.value);
+    debouncedCallback(e.target.value);
+  };
+
   return (
     <div>
+      <div>
+        <input type='text' value={value} onChange={handleChange} />
+      </div>
       {/* useInput */}
       <div style={{ margin: 50 }}>
         <input {...Username} type='text' placeholder='Username' />
