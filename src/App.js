f014b63cda46c6f66204c0ddcdeedd9c349d871d
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-01 21:47:41 +0300

    feat: add useRequest

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import './App.css';
-import React, { useState } from 'react';
-import useInput from './hooks/useInput';
-import Hover from './components/Hover';
-import List from './components/List';
-import useDebounce from './hooks/useDebounce';
-
+import React from 'react';
+import axios from 'axios';
+import useRequest from './hooks/useRequest';
 function App() {
-  const Username = useInput('');
-  const Password = useInput('');
+  const [todos, loading, error] = useRequest(fetchTodos);
 
-  const [value, setValue] = useState('');
-  const debouncedCallback = useDebounce(search, 500);
-  function search(query) {
-    fetch(`https://jsonplaceholder.typicode.com/todos` + query)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-      });
+  function fetchTodos() {
+    return axios.get(`https://jsonplaceholder.typicode.com/todos`);
   }
 
-  const handleChange = e => {
-    setValue(e.target.value);
-    debouncedCallback(e.target.value);
-  };
+  if (loading) {
+    return <h1>Loading....</h1>;
+  }
+
+  if (error) {
+    return <h1>Error </h1>;
+  }
 
   return (
     <div>
-      <div>
-        <input type='text' value={value} onChange={handleChange} />
-      </div>
-      {/* useInput */}
-      <div style={{ margin: 50 }}>
-        <input {...Username} type='text' placeholder='Username' />
-        <input {...Password} type='password' placeholder='Password' />
-        <button onClick={() => console.log(Username.value, Password.value)}>Click</button>
-      </div>
-      {/* useHover */}
-      <Hover />
-      <List />
+      {todos &&
+        todos.map(todo => (
+          <div key={todo.id} style={{ padding: 30, border: '2px solid black' }}>
+            {todo.id}. {todo.title}
+          </div>
+        ))}
     </div>
   );
 }
